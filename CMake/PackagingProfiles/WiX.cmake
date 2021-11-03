0c0099973cb82f9a9cb98cd5604f590b02c8687b
CC <12-5669782@qq.com>
2021-11-03 14:31:25 +0100

    Wix package

---
+++ CMake/PackagingProfiles/WiX.cmake
@@ -0,0 +1,23 @@
+#
+#  PackagingProfiles/WiX.cmake
+#  Copyright 2021 ItJustWorksTM
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+set (CPACK_GENERATOR WIX)
+#Generate by online website : https://www.guidgenerator.com/online-guid-generator.aspx
+set (CPACK_WIX_UPGRADE_GUID C32F838C-EBF1-42D1-AC8C-DB918F25CD94)
+set (CPACK_WIX_ROOT_FEATURE_TITLE "SMCE libSMCE")
+#set (CPACK_WIX_PRODUCT_ICON "${PROJECT_SOURCE_DIR}/project/media/images/icon.png")
+set (CPACK_WIX_PATH)
