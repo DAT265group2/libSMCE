6c6c031a90bc2bf18d839e8bd065b055cd93613b
zhiyanz <zzyjoy1997@gmail.com>
2021-12-02 23:48:58 +0100

    test write0

--- test/BoardView.cpp
+++ test/BoardView.cpp
@@ -175,7 +175,7 @@ TEST_CASE("BoardView Blocking I/O", "[BoardView]"){
     std::array out = {'H', 'E', 'L', 'L', 'O', ' ', 'U', 'A', 'R', 'T', '\0'};
     std::array<char, out.size()> in{};
 
-    uart0.rx().blocking_write(out);
+    REQUIRE(uart0.rx().blocking_write(out) == out.size());
 //    REQUIRE(uart0.tx().blocking_read(in) == in.size());
 
     /*std::thread task_read {[&]{
