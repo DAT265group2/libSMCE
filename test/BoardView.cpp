dccc7c816f4511324cecf839fee63913d0f19312
zhiyanz <zzyjoy1997@gmail.com>
2021-12-03 00:37:30 +0100

    test read&write4

--- test/BoardView.cpp
+++ test/BoardView.cpp
@@ -176,8 +176,7 @@ TEST_CASE("BoardView Blocking I/O", "[BoardView]"){
     std::array<char, out.size()> in{};
     REQUIRE(uart0.rx().blocking_write(out) == out.size());
     REQUIRE(uart0.rx().size() == in.size());
-    REQUIRE(uart0.tx().size() == in.size());
-    REQUIRE(uart0.tx().read(in) == in.size());
+    REQUIRE(uart0.rx().read(in) == in.size());
 
     /*std::thread task_read {[&]{
         REQUIRE(uart0.rx().read(in) == in.size());
